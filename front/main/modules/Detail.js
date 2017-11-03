0e8e56e11447ccad37b3389b68e7796402680859
qiliangya <871259169@qq.com>
2017-11-03 19:51:54 +0800

    由于数据库传过来的日期是TZ格式的，进行了changeDate自定义方法转换

--- front/main/modules/Detail.js
+++ front/main/modules/Detail.js
@@ -10,6 +10,17 @@ class Detail extends React.Component{
         super();
         this.newsData = '';
     }
+    changeDate(date){       //需要一个实例化日期对象
+        var y = date.getFullYear();  
+        var m = date.getMonth() + 1;  
+        m = m < 10 ? ('0' + m) : m;  
+        var d = date.getDate();  
+        d = d < 10 ? ('0' + d) : d;  
+        var h = date.getHours();  
+        var minute = date.getMinutes();  
+        minute = minute < 10 ? ('0' + minute) : minute;  
+        return y + '-' + m + '-' + d+' '+h+':'+minute;  
+    }
     componentWillMount(){
         const {allNewsList,params} = this.props;
         const parId = params.id;
@@ -31,7 +42,7 @@ class Detail extends React.Component{
                     <h3 className="text-center">{title}</h3>
                     <p>{subtitle}</p>
                     <p>{author}</p>
-                    <p>{newsdate}</p>
+                    <p>{this.changeDate(new Date(newsdate))}</p>
                     <p>{content}</p>
                 </div>
             )
